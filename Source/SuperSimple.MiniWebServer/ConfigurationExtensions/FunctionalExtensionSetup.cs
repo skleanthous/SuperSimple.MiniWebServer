da303f6d1e65e66b28224ed474c98476ce7135f4
Savvas Kleanthous <skleanthous@outlook.com>
2018-07-20 11:51:17 +0100

    Added funcional controller setup methods for HttpContent

--- Source/SuperSimple.MiniWebServer/ConfigurationExtensions/FunctionalExtensionSetup.cs
+++ Source/SuperSimple.MiniWebServer/ConfigurationExtensions/FunctionalExtensionSetup.cs
@@ -1,6 +1,7 @@
 ﻿namespace SuperSimple.MiniWebServer
 {
     using System;
+    using System.Net.Http;
     using SuperSimple.MiniWebServer.MiddleWare.ControllerFunction;
     using SuperSimple.MiniWebServer.Response;
 
@@ -9,7 +10,9 @@
         public static IMiddlewareSetup AddControllerFunction(this IMiddlewareSetup middlewareSetup, string path, Func<Request, object> controllerFunc)
             => middlewareSetup.AddControllerFunction( req => req.RequestPath.Equals(path, StringComparison.InvariantCultureIgnoreCase), controllerFunc);
 
-        public static IMiddlewareSetup AddControllerFunction(this IMiddlewareSetup middlewareSetup, Func<Request, bool> controllerFunctionCanHandleRequest, Func<Request, object> controllerFunc)
+        public static IMiddlewareSetup AddControllerFunction(this IMiddlewareSetup middlewareSetup,
+            Func<Request, bool> controllerFunctionCanHandleRequest,
+            Func<Request, object> controllerFunc)
         {
             var controllerFunctionMiddleware = new ControllerFunctionMiddleware(controllerFunctionCanHandleRequest,
                 req =>
@@ -22,5 +25,21 @@
 
             return middlewareSetup;
         }
+
+        public static IMiddlewareSetup AddControllerFunctionThatReturnsHttpContent(
+            this IMiddlewareSetup middlewareSetup,
+            Func<Request, bool> canHandle,
+            Func<Request, HttpContent> controllerFunc)
+            => AddControllerFunction(middlewareSetup, canHandle, controllerFunc);
+
+        public static IMiddlewareSetup AddControllerFunctionThatReturnsHttpContent(this IMiddlewareSetup middlewareSetup,
+            Func<Request, bool> canHandle,
+            Func<Request, (HttpContent content, int returnCode)> controllerFunc)
+            => AddControllerFunction(middlewareSetup, canHandle, req =>
+            {
+                var response = controllerFunc(req);
+
+                return new ControllerReply(response.content, response.returnCode);
+            });
     }
 }
